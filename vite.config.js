3707bc13de2d09980bbd0d63a4adea3970e38af3
el-zaelani <ezaelani72@gmail.com>
2023-05-18 08:05:27 +0700

    ediit vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-portofolio",
   plugins: [react()],
   server: {
     port: 8001,
