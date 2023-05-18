7a31d713e27e250d66104af58170bbb2e634caff
el-zaelani <ezaelani72@gmail.com>
2023-05-18 07:13:57 +0700

    update portofolo

---
+++ src/components/Bot.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Bot = () => {
+  return (
+    <div className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-cyan-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce">
+      <ion-icon name="chatbubble-ellipses">
+        <a href="https://wa.me/+6281323513371"></a>
+      </ion-icon>
+    </div>
+  );
+};
+
+export default Bot;
